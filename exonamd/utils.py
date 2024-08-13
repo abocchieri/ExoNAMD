bf11f79d93002af5bb30e8f2e41daae1f67a55fa
andrea.bocchieri <andrea.bocchieri@uniroma1.it>
2024-08-13 20:46:26 +0200

    saving progress with inputting values

--- exonamd/utils.py
+++ exonamd/utils.py
@@ -7,6 +7,8 @@ import numpy as np
 import requests
 import warnings
 from astroquery.simbad import Simbad as simbad
+from astropy import constants as cc
+from astropy import units as u
 
 
 ROOT = os.path.realpath(os.path.dirname(__file__)) + '/'
@@ -436,3 +438,109 @@ def update_planet(row, aliases, verbose=False):
                 if verbose: print(f"Found {planet} in aliases, updating to {name}")
                 return name
     return planet
+
+
+def solve_a_rs(sma, rstar, ars):
+    """
+    Solve semi-major axis -- stellar radius system of equations.
+
+    Parameters
+    ----------
+    sma: Float
+        Orbital semi-major axis (AU).
+    rstar: Float
+        Stellar radius (r_sun).
+    ars: Float
+        sma--rstar ratio.
+    """
+    missing = (
+        np.isnan(sma) +
+        np.isnan(ars) +
+        np.isnan(rstar)
+    )
+    # Know everything or not enough:
+    if missing != 1:
+       return sma, rstar, ars
+
+    if np.isnan(sma):
+        sma = ars * rstar*cc.R_sun / cc.au
+        sma = sma.value
+    elif np.isnan(rstar):
+        rstar = sma*cc.au / ars
+        rstar = rstar.to(u.R_sun).value
+    elif np.isnan(ars):
+        ars = sma*cc.au / (rstar*cc.R_sun)
+        ars = ars.value
+
+    return sma, rstar, ars
+
+
+def solve_rprs(rplanet, rstar, rprs):
+    """
+    Solve planet radius -- stellar radius system of equations.
+
+    Parameters
+    ----------
+    rplanet: Float
+        Planet radius (r_earth).
+    rstar: Float
+        Stellar radius (r_sun).
+    rprs: Float
+        Planet--star radius ratio.
+    """
+    missing = (
+        np.isnan(rplanet) +
+        np.isnan(rstar) +
+        np.isnan(rprs)
+    )
+    # Know everything or not enough:
+    if missing != 1:
+        return rplanet, rstar, rprs
+
+    if np.isnan(rplanet):
+        rplanet = rprs * (rstar*cc.R_sun)
+        rplanet = rplanet.to(u.R_earth).value
+    elif np.isnan(rstar):
+        rstar = rplanet*cc.R_earth / rprs
+        rstar = rstar.to(u.R_sun).value
+    elif np.isnan(rprs):
+        rprs = rplanet*cc.R_earth / (rstar*cc.R_sun)
+        rprs = rprs.value
+
+    return rplanet, rstar, rprs
+
+
+def solve_a_period(period, sma, mstar):
+    """
+    Solve period-sma-mstar system of equations.
+
+    Parameters
+    ----------
+    period: Float
+        Orbital period (days).
+    sma: Float
+        Orbital semi-major axis (AU).
+    mstar: Float
+        Stellar mass (m_sun).
+    """
+    missing = (
+        np.isnan(period) +
+        np.isnan(sma) +
+        np.isnan(mstar)
+    )
+    # Know everything or not enough:
+    if missing != 1:
+        return period, sma, mstar
+
+    two_pi_G = 2.0*np.pi / np.sqrt(cc.G)
+    if np.isnan(mstar):
+        mstar = (sma*cc.au)**3.0 / (period*u.day/two_pi_G)**2.0
+        mstar = mstar.to(u.M_sun).value
+    elif np.isnan(period):
+        period = np.sqrt((sma*cc.au)**3.0 / (mstar*cc.M_sun)) * two_pi_G
+        period = period.to(u.day).value
+    elif np.isnan(sma):
+        sma = ((period*u.day/two_pi_G)**2.0 * (mstar*cc.M_sun))**(1/3) / cc.au
+        sma = sma.value
+
+    return period, sma, mstar
