b6c5668ac0fccb3cce9b8e58d60cfd4870aaf632
Andrea Bocchieri <58187203+abocchieri@users.noreply.github.com>
2024-08-16 22:21:47 +0200

    implemented pop_plot

--- exonamd/plot.py
+++ exonamd/plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def simple_plot(
     data,
     kind,
-    title,
+    title="",
     which="namd",
     ylabel="Frequency",
     scale="linear",
@@ -45,4 +45,84 @@ def simple_plot(
         color=["red", "black", "red"],
         linestyles="dashed",
     )
+    plt.show()
+
+
+def pop_plot(df, kind, title="", which="namd", yscale="log", xoffs=False):
+    # Plot the values vs multiplicity and color by their relative uncertainty
+
+    df = df.sort_values(by="sy_pnum")
+    sy_pnum = df["sy_pnum"]
+    q50 = df[f"{which}_{kind}_q50"]
+    q16 = df[f"{which}_{kind}_q16"]
+    q84 = df[f"{which}_{kind}_q84"]
+
+    nanidx = q50.isnull()
+    sy_pnum = sy_pnum[~nanidx]
+    q50 = q50[~nanidx]
+    q16 = q16[~nanidx]
+    q84 = q84[~nanidx]
+
+    errup = q84 - q50
+    errdown = q50 - q16
+    iq = (q84 - q16) / 2
+    sigma_rel = iq / q50
+
+    ylabel = rf"{which.upper()}$_{kind[0].upper()}$"
+
+    coeffs = np.polyfit(sy_pnum, q50, 1)
+    line = np.polyval(coeffs, np.array(list(set(sy_pnum))))
+    if yscale == "log":
+        coeffs = np.polyfit(sy_pnum, np.log10(q50), 1)
+        line = 10 ** np.polyval(coeffs, np.array(list(set(sy_pnum))))
+
+    plt.figure()
+    plt.plot(
+        np.array(list(set(sy_pnum))),
+        line,
+        "k--",
+        alpha=0.5,
+        lw=1.5,
+        zorder=10,
+    )
+
+    if xoffs:
+        M = set(sy_pnum)
+        n_list = []
+        for m in M:
+            idx = sy_pnum == m
+            n_list.append(idx.sum())
+        n_list = np.array(n_list)
+        xoffs = 0.3 * (n_list / n_list.max()) ** 1.5
+        for i, m in enumerate(M):
+            idx = sy_pnum == m
+            sy_pnum[idx] += np.linspace(-xoffs[i], xoffs[i], idx.sum())
+
+    plt.errorbar(
+        sy_pnum,
+        q50,
+        yerr=[errdown, errup],
+        fmt="none",
+        c="k",
+        alpha=0.5,
+        lw=0.5,
+        capsize=2,
+    )
+    s = plt.scatter(
+        sy_pnum,
+        q50,
+        c=sigma_rel,
+        cmap="coolwarm",
+        zorder=9,
+    )
+    plt.colorbar(s, label="Relative uncertainty")
+    plt.clim(0, 1)
+    plt.xlabel("Multiplicity")
+    plt.ylabel(ylabel)
+    plt.yscale(yscale)
+    plt.title(title)
+
+    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(integer=True))
+    plt.grid(which="both", linestyle="--", alpha=0.5)
+
     plt.show()
