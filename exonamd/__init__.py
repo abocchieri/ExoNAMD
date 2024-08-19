01899fb2d650661f2beb265e6823f1baf533654d
andrea.bocchieri <andrea.bocchieri@uniroma1.it>
2024-08-19 22:57:13 +0200

    minor fixes

--- exonamd/__init__.py
+++ exonamd/__init__.py
@@ -5,9 +5,7 @@ from .__version__ import __version__
 
 # load package info
 __pkg_name__ = metadata.metadata("exonamd")["Name"]
-__url__ = metadata.metadata("exonamd")["Home-page"]
 __author__ = metadata.metadata("exonamd")["Author"]
-__email__ = metadata.metadata("exonamd")["Author_email"]
 __license__ = metadata.metadata("exonamd")["license"]
 __copyright__ = "2024-{:d}, {}".format(date.today().year, __author__)
 __summary__ = metadata.metadata("exonamd")["Summary"]
